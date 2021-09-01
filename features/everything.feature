f768282bbd13720e3151adab8eacfcb89e151bc7
Djonatas Borges <interdjonatas@gmail.com>
2021-09-01 18:13:18 -0300

    Implementando o Then

--- features/everything.feature
+++ features/everything.feature
@@ -2,4 +2,4 @@ Feature: Fazer uma compra
   Scenario: Tela de login
     Given que eu esteja na página de login
     When preencher os dados de login
-
+    Then Então devo logar no site com sucesso.
--- features/steps/steps.py
+++ features/steps/steps.py
@@ -1,24 +1,23 @@
 from behave import given, when, then
-from selenium import webdriver
 from pages.login import Login
 from time import sleep
 
 
 @given(u'que eu esteja na página de login')
-def step(context):
+def page_login(context):
     context.driver.get('https://www.saucedemo.com/')
     sleep(3)
 
 
 @when(u'preencher os dados de login')
-def step_login(context):
+def log_in(context):
     login = Login(context.driver)
     login.log_in()
     sleep(3)
 
 
 @then(u'Então devo logar no site com sucesso.')
-def step_p(context):
+def confirmed_login(context):
     try:
         if context.driver.current_url == 'https://www.saucedemo.com/inventory.html':
             print('login efetuado com sucesso.')
